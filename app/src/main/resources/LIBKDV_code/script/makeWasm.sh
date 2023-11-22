87d7c8cc95c1899b1acab6162ab12cbcdfb841ce
Juan <2215736942@qq.com>
2023-11-22 16:38:19 +0800

    change sh

--- app/src/main/resources/LIBKDV_code/script/makeWasm.sh
+++ app/src/main/resources/LIBKDV_code/script/makeWasm.sh
@@ -1,2 +1,2 @@
 cd ..
-emcc -lembind -o target/kdvCpp.js alg_visual.cpp baseline.cpp bucket.cpp EDWIN_multiple.cpp EDWIN_otf.cpp init_visual.cpp main.cpp SLAM.cpp SWS.cpp -sEXIT_RUNTIME --preload-file data/cases.csv
+emcc -lembind -o ../static/js/kdv/kdvCpp.js alg_visual.cpp baseline.cpp bucket.cpp EDWIN_multiple.cpp EDWIN_otf.cpp init_visual.cpp main.cpp SLAM.cpp SWS.cpp -sEXIT_RUNTIME --preload-file data/cases.csv
