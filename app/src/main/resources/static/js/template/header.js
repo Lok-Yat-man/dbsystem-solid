76e3668e027e9d603c9ee586cc04b14885b0a0a4
Juan <2215736942@qq.com>
2024-03-05 14:09:18 +0800

    顶栏文字排版修改，参数列表大小修改，DCPGS进度条完成

--- app/src/main/resources/static/js/template/header.js
+++ app/src/main/resources/static/js/template/header.js
@@ -8,15 +8,15 @@ let db_header = Vue.extend({
                 <div class="ename">Spatial-Temporal Analytics with Rapid System</div>
             </div>
             <div class="title2">
-                <div id="school">
-                    <span class="cname">深圳大学</span>
-                </div>
+<!--                <div id="school">-->
+<!--                    <span class="cname">深圳大学</span>-->
+<!--                </div>-->
                 <div id="college">
                     <span class="cname">深圳大学计算机与软件学院</span>
                 </div>
-                <div id="lab">
-                    <span class="cname">大数据与应用数据研究所</span>
-                </div>
+<!--                <div id="lab">-->
+<!--                    <span class="cname">大数据与应用数据研究所</span>-->
+<!--                </div>-->
             </div>
         </div>
     </div>
